5470cc53255680ce993dcd3da78fe1739402b2c8
deerdesignerdevops <devops@deerdesigner.com>
2023-11-09 18:31:34 -0300

    creative calls test button

--- functions.php
+++ functions.php
@@ -393,15 +393,14 @@ function checkIfUserCanBookCreativeCall(){
 	$canUserBookACreativeCall =  get_user_meta(get_current_user_id(), 'stripe_customer_plan', true);
 	$remainingCalls =  get_user_meta(get_current_user_id(), 'creative_calls', true);
 
-	if(str_contains($canUserBookACreativeCall, 'Agency') || $remainingCalls){
+	if($remainingCalls || str_contains($canUserBookACreativeCall, 'Agency')){
 		echo "<style>.book_call_btn{display: flex !important;}</style>";
 	}else{
 		echo "<style>.book_call_btn{display: none !important;}</style>";
 	}
 
 }
-
-//add_action('template_redirect', 'checkIfUserCanBookCreativeCall');
+add_action('template_redirect', 'checkIfUserCanBookCreativeCall');
 
 
 
