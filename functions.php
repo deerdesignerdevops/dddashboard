b6784975e6dc3809385a26bfaac51f4c9c5629e2
deerdesignerdevops <devops@deerdesigner.com>
2023-12-12 23:27:47 -0300

    disable slack notification

--- functions.php
+++ functions.php
@@ -629,7 +629,7 @@ function sendUserOnboardedNotificationFromWooToSlack($entryId, $formData, $form)
 		slackNotifications($slackMessageBody);
 	}
 }
-add_action( 'fluentform/submission_inserted', 'sendUserOnboardedNotificationFromWooToSlack', 10, 3);
+//add_action( 'fluentform/submission_inserted', 'sendUserOnboardedNotificationFromWooToSlack', 10, 3);
 
 
 
