8896dd6c71e20ffc524a3425aa3be06601210048
deerdesignerdevops <devops@deerdesigner.com>
2023-12-22 08:57:42 -0300

    disable slack notification for subs update

--- functions.php
+++ functions.php
@@ -984,7 +984,7 @@ function notificationToSlackWithSubscriptionUpdateStatus($subscription, $new_sta
 		slackNotifications($slackMessageBody);
 	}
 }
-add_action('woocommerce_subscription_status_updated', 'notificationToSlackWithSubscriptionUpdateStatus', 10, 3);
+//add_action('woocommerce_subscription_status_updated', 'notificationToSlackWithSubscriptionUpdateStatus', 10, 3);
 
 
 
