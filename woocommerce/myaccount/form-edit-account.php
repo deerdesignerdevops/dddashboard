92d49699fef35f73ed544a90a4854cbdf607dc01
Marcus Freitas <marcusfreitasantos@gmail.com>
2023-12-28 12:25:45 -0300

    change name and position of content

--- woocommerce/myaccount/form-edit-account.php
+++ woocommerce/myaccount/form-edit-account.php
@@ -35,131 +35,140 @@ fieldset {
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <section>
-	<a href="/" class="dd__bililng_portal_back"><i class="fa-solid fa-chevron-left"></i> Back to Dashboard</a>
-	<h1 class="myaccount__page_title">Edit Account</h1>
-</section>
+	<a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' )); ?>" class="dd__bililng_portal_back"><i class="fa-solid fa-chevron-left"></i> Back to Dashboard</a>
+	
+	<div class="account__details_row">
+		<div class="account__details_col">
+			<h2 class="myaccount__page_title">Your details</h2>
 
-<form class="woocommerce-EditAccountForm edit-account" action="" method="post" <?php do_action( 'woocommerce_edit_account_form_tag' ); ?> >
+			<form class="woocommerce-EditAccountForm edit-account" action="" method="post" <?php do_action( 'woocommerce_edit_account_form_tag' ); ?> >
+				<?php do_action( 'woocommerce_edit_account_form_start' ); ?>
 
-	<?php do_action( 'woocommerce_edit_account_form_start' ); ?>
+				<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
+					<label for="account_first_name"><?php esc_html_e( 'First name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+					<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr( $user->first_name ); ?>" />
+				</p>
+				<p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
+					<label for="account_last_name"><?php esc_html_e( 'Last name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+					<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr( $user->last_name ); ?>" />
+				</p>
+				<div class="clear"></div>
 
-	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-		<label for="account_first_name"><?php esc_html_e( 'First name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr( $user->first_name ); ?>" />
-	</p>
-	<p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
-		<label for="account_last_name"><?php esc_html_e( 'Last name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr( $user->last_name ); ?>" />
-	</p>
-	<div class="clear"></div>
+				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+					<label for="account_display_name"><?php esc_html_e( 'Display name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+					<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $user->display_name ); ?>" /> <span><em><?php esc_html_e( 'This will be how your name will be displayed in the account section and in reviews', 'woocommerce' ); ?></em></span>
+				</p>
+				<div class="clear"></div>
 
-	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-		<label for="account_display_name"><?php esc_html_e( 'Display name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $user->display_name ); ?>" /> <span><em><?php esc_html_e( 'This will be how your name will be displayed in the account section and in reviews', 'woocommerce' ); ?></em></span>
-	</p>
-	<div class="clear"></div>
+				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+					<label for="account_email"><?php esc_html_e( 'Email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+					<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr( $user->user_email ); ?>" />
+				</p>
 
-	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-		<label for="account_email"><?php esc_html_e( 'Email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr( $user->user_email ); ?>" />
-	</p>
+				<fieldset>
+					<legend><?php esc_html_e( 'Password change', 'woocommerce' ); ?></legend>
 
-	<fieldset>
-		<legend><?php esc_html_e( 'Password change', 'woocommerce' ); ?></legend>
+					<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+						<label for="password_current"><?php esc_html_e( 'Current password (leave blank to leave unchanged)', 'woocommerce' ); ?></label>
+						<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_current" id="password_current" autocomplete="off" />
+					</p>
+					<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+						<label for="password_1"><?php esc_html_e( 'New password (leave blank to leave unchanged)', 'woocommerce' ); ?></label>
+						<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_1" id="password_1" autocomplete="off" />
+					</p>
+					<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+						<label for="password_2"><?php esc_html_e( 'Confirm new password', 'woocommerce' ); ?></label>
+						<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_2" id="password_2" autocomplete="off" />
+					</p>
+				</fieldset>
+				<div class="clear"></div>
 
-		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-			<label for="password_current"><?php esc_html_e( 'Current password (leave blank to leave unchanged)', 'woocommerce' ); ?></label>
-			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_current" id="password_current" autocomplete="off" />
-		</p>
-		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-			<label for="password_1"><?php esc_html_e( 'New password (leave blank to leave unchanged)', 'woocommerce' ); ?></label>
-			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_1" id="password_1" autocomplete="off" />
-		</p>
-		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-			<label for="password_2"><?php esc_html_e( 'Confirm new password', 'woocommerce' ); ?></label>
-			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_2" id="password_2" autocomplete="off" />
-		</p>
-	</fieldset>
-	<div class="clear"></div>
+				<?php do_action( 'woocommerce_edit_account_form' ); ?>
 
-	<?php do_action( 'woocommerce_edit_account_form' ); ?>
+				<p>
+					<?php wp_nonce_field( 'save_account_details', 'save-account-details-nonce' ); ?>
+					<button type="submit" class="woocommerce-Button button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?> dd__primary_button" name="save_account_details" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>"><?php esc_html_e( 'Save changes', 'woocommerce' ); ?></button>
+					<input type="hidden" name="action" value="save_account_details" />
+				</p>
 
-	<p>
-		<?php wp_nonce_field( 'save_account_details', 'save-account-details-nonce' ); ?>
-		<button type="submit" class="woocommerce-Button button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?> dd__primary_button" name="save_account_details" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>"><?php esc_html_e( 'Save changes', 'woocommerce' ); ?></button>
-		<input type="hidden" name="action" value="save_account_details" />
-	</p>
+				<?php do_action( 'woocommerce_edit_account_form_end' ); ?>
+			</form>
 
-	<?php do_action( 'woocommerce_edit_account_form_end' ); ?>
-</form>
+			<?php do_action( 'woocommerce_after_edit_account_form' ); ?>
+		</div>
 
-<?php do_action( 'woocommerce_after_edit_account_form' ); ?>
+		<div class="account__details_col">
+			<?php 
+			//PAYMENT METHODS
+			$saved_methods = wc_get_customer_saved_methods_list( get_current_user_id() );
+			$has_methods   = (bool) $saved_methods;
+			$types         = wc_get_account_payment_methods_types();
 
+			do_action( 'woocommerce_before_account_payment_methods', $has_methods ); ?>
 
-<?php 
-//PAYMENT METHODS
-$saved_methods = wc_get_customer_saved_methods_list( get_current_user_id() );
-$has_methods   = (bool) $saved_methods;
-$types         = wc_get_account_payment_methods_types();
+			<?php if ( $has_methods ) : ?>
 
-do_action( 'woocommerce_before_account_payment_methods', $has_methods ); ?>
-
-<?php if ( $has_methods ) : ?>
-
-<section class="payment__methods_section" style="margin-top: 40px;">
-	<h2 class="dd__billing_portal_section_title">Payment Methods</h2>
-		
-	<?php foreach ( $saved_methods as $type => $methods ) :  ?>
-		<?php foreach ( $methods as $method ) : ?>
-			<div class="user__invoice_row">
-					<?php foreach ( wc_get_account_payment_methods_columns() as $column_id => $column_name ) : ?>
-							<?php
-								if ( has_action( 'woocommerce_account_payment_methods_column_' . $column_id ) ) {
-									do_action( 'woocommerce_account_payment_methods_column_' . $column_id, $method );
-								} elseif ( 'method' === $column_id ) { ?>
-									<span>
+				<h2 class="myaccount__page_title">Payment Methods</h2>
+					
+				<?php foreach ( $saved_methods as $type => $methods ) :  ?>
+					<?php foreach ( $methods as $method ) : ?>
+						<div class="user__invoice_row">
+								<?php foreach ( wc_get_account_payment_methods_columns() as $column_id => $column_name ) : ?>
 										<?php
-											if ( ! empty( $method['method']['last4'] ) ) {
-												echo sprintf( esc_html__( '%1$s ending in %2$s', 'woocommerce' ), esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) ), esc_html( $method['method']['last4'] ) );
-											} else {
-												echo esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) );
-											}
-										?>
-									
-										<?php
-										} elseif ( 'expires' === $column_id ) {
-											echo ' - ' . esc_html( $method['expires'] );
-										?>
+											if ( has_action( 'woocommerce_account_payment_methods_column_' . $column_id ) ) {
+												do_action( 'woocommerce_account_payment_methods_column_' . $column_id, $method );
+											} elseif ( 'method' === $column_id ) { ?>
+												<span>
+													<?php
+														if ( ! empty( $method['method']['last4'] ) ) {
+															echo sprintf( esc_html__( '%1$s ending in %2$s', 'woocommerce' ), esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) ), esc_html( $method['method']['last4'] ) );
+														} else {
+															echo esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) );
+														}
+													?>
+												
+													<?php
+													} elseif ( 'expires' === $column_id ) {
+														echo ' - ' . esc_html( $method['expires'] );
+													?>
 
-									</span>
-								<?php
-								} elseif ( 'actions' === $column_id ) { ?>
-									<div style="display: flex; gap: 12px;">
+												</span>
+											<?php
+											} elseif ( 'actions' === $column_id ) { ?>
+												<div style="display: flex; gap: 12px;">
 
-									<?php
-									foreach ( $method['actions'] as $key => $action ) {
-										echo '<a href="' . esc_url( $action['url'] ) . '" >' . esc_html( $action['name'] ) . '</a>';
-									}
-									?>
-									</div>
-								<?php }
-							?>
-					<?php endforeach; ?>
+												<?php
+												foreach ( $method['actions'] as $key => $action ) {
+													echo '<a href="' . esc_url( $action['url'] ) . '" >' . esc_html( $action['name'] ) . '</a>';
+												}
+												?>
+												</div>
+											<?php }
+										?>
+								<?php endforeach; ?>
+							</div>
+						<?php endforeach; ?>
+				<?php endforeach; ?>
+
+			<?php else : ?>
+
+				<?php wc_print_notice( esc_html__( 'No saved payment methods found.', 'woocommerce' ), 'notice' ); ?>
+
+			<?php endif; ?>
+
+			<?php do_action( 'woocommerce_after_account_payment_methods', $has_methods ); ?>
+
+			<?php if ( WC()->payment_gateways->get_available_payment_gateways() ) : ?>
+				<div style="margin-top: 20px;">
+					<a class="dd__primary_button" href="<?php echo esc_url( wc_get_endpoint_url( 'add-payment-method' ) ); ?>"><?php esc_html_e( 'Add payment method', 'woocommerce' ); ?></a>
 				</div>
-			<?php endforeach; ?>
-	<?php endforeach; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+
 </section>
 
-<?php else : ?>
 
-	<?php wc_print_notice( esc_html__( 'No saved payment methods found.', 'woocommerce' ), 'notice' ); ?>
 
-<?php endif; ?>
 
-<?php do_action( 'woocommerce_after_account_payment_methods', $has_methods ); ?>
 
-<?php if ( WC()->payment_gateways->get_available_payment_gateways() ) : ?>
-	<div style="margin-top: 20px;">
-		<a class="dd__primary_button" href="<?php echo esc_url( wc_get_endpoint_url( 'add-payment-method' ) ); ?>"><?php esc_html_e( 'Add payment method', 'woocommerce' ); ?></a>
-	</div>
-<?php endif; ?>
