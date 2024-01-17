44ed49447b3504190b4ba6a267e1c1787c55ef38
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-01-17 15:13:13 -0300

    verify subscription status before moosend request

--- integrations/moosend.php
+++ integrations/moosend.php
@@ -31,6 +31,21 @@ add_action( 'fluentform/submission_inserted', 'subscribeUserToMoosendEmailList',
 
 
 
+function scheduleMoosendUpdateStatus($subscriptionId, $newStatus, $userName, $userEmail, $status){
+	$subscription = wcs_get_subscription($subscriptionId);
+	
+	if($subscription->get_status() === $newStatus){		
+		foreach($subscription->get_items() as $subscritpionItem){
+			if(has_term('plan', 'product_cat', $subscritpionItem['product_id'])){
+				curlToMoosend($userName, $userEmail, $status);
+			}
+		}
+	}
+}
+add_action('scheduleMoosendUpdateStatusHook', 'scheduleMoosendUpdateStatus', 10, 5);
+
+
+
 function updateUserInMoosendBasedOnSubscriptionStatus($subscription, $newStatus, $oldStatus){
 	if(isset($_GET['change_subscription_to']) || isset($_GET['reactivate_plan'])){;
 		if($oldStatus !== 'pending' && $newStatus !== 'cancelled'){
@@ -39,30 +54,30 @@ function updateUserInMoosendBasedOnSubscriptionStatus($subscription, $newStatus,
 					$billingPeriodEndingDate =  strtotime(calculateBillingEndingDateWhenPausedOrCancelled($subscription));
 					$userName = $subscription->data['billing']['first_name'] . " " . $subscription->data['billing']['last_name'];
 					$userEmail = $subscription->data['billing']['email'];
-					$status = "";
+					$moosendUserNewStatus = "";
 
 					switch($newStatus){
 						case "active":
-							$status = "active";
+							$moosendUserNewStatus = "active";
 							break;
 						
 						case "on-hold":
-							$status = "paused";
+							$moosendUserNewStatus = "paused";
 							break;
 						
 						case "pending-cancel":
-							$status = "cancelled";
+							$moosendUserNewStatus = "cancelled";
 							break;
 						
 						default:
-							$status = "paused";
+							$moosendUserNewStatus = "paused";
 					}
 
 					
 					if(time() < $billingPeriodEndingDate){
-						wp_schedule_single_event($billingPeriodEndingDate, 'curlToMoosendHook', array($userName, $userEmail, $status));
+						wp_schedule_single_event($billingPeriodEndingDate, 'scheduleMoosendUpdateStatusHook', array($subscription->id, $newStatus, $userName, $userEmail, $moosendUserNewStatus));
 					}else{
-						curlToMoosend($userName, $userEmail, $status);
+						curlToMoosend($userName, $userEmail, $moosendUserNewStatus);
 					}
 				}
 			}
