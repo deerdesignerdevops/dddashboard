a63c4bcb73baa4d558aea68910a5294a8caf8b68
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-01-24 10:36:18 -0300

    moosend integration logs

--- integrations/moosend.php
+++ integrations/moosend.php
@@ -1,6 +1,7 @@
 <?php
 
 function curlToMoosend($userName, $userEmail, $status, $moosendList){
+	$uploadsDir = wp_upload_dir()['basedir'] . '/integrations-api-logs';
 	$moosendApiUrl = $moosendList === "news" ? MOOSEND_API_URL_NEWS : MOOSEND_API_URL;
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $moosendApiUrl);
@@ -12,9 +13,21 @@ function curlToMoosend($userName, $userEmail, $status, $moosendList){
 	]);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, "{\n    \"Name\" : \"$userName\",\n    \"Email\" : \"$userEmail\",\n    \"HasExternalDoubleOptIn\": false,\n\"CustomFields\":[\n \"status=$status\"]}");
 
-	curl_exec($ch);
+	$response = curl_exec($ch);
+
+	if (curl_errno($ch)) {
+		$error_message = 'Error: ' . curl_error($ch);
+		echo $error_message;
+		error_log($error_message, 3, "$uploadsDir/moosend_api_error_log.txt");
+		$response = false;
+	} else {
+		file_put_contents("$uploadsDir/moosend_api_response_log_post_request.txt", $response . PHP_EOL, FILE_APPEND);
+		$response = json_decode($response, true);
+	}
 
 	curl_close($ch);
+
+	return $response;
 }
 
 
