84b1dd85f3918af0542898fe51d2aeeaba49edd4
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-02-20 16:56:13 -0300

    remove discount from referrer subscription after renewal

--- integrations/growsurf.php
+++ integrations/growsurf.php
@@ -185,3 +185,15 @@ function applyDiscountToReferrerNextRenewal($referrerId){
 		}
 	}
 }
+
+
+function removeDiscountFromSubscriptionAfterPayment($subscription, $lastOrder){
+    $couponCode = 'deerreferrer';
+    $coupons = $subscription->get_used_coupons();
+    
+    if(in_array($couponCode, $coupons)){
+        $subscription->remove_coupon( $couponCode );
+        $subscription->save();
+    }
+}
+add_action( 'woocommerce_subscription_renewal_payment_complete', 'removeDiscountFromSubscriptionAfterPayment', 10, 2 );
