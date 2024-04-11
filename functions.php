437a323703cc6651f4105c0258a331e18686f7c4
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-04-11 11:57:29 -0300

    check if user is logged to redirect to affiliate page

--- functions.php
+++ functions.php
@@ -1880,21 +1880,22 @@ add_action('resetCreativeCallsForBusinessAnnualActiveUsersHook', 'resetCreativeC
 
 //AFFILIATE PROGRAM
 function redirectUserToAffiliatesPanel(){
-	$currentUser = wp_get_current_user();
-	if(sizeof($currentUser->roles) === 1 && in_array('affiliate', $currentUser->roles)){
-		echo "<style>
-			.paused__user_banner{display:none !important;}
-			.account_details__section{width: 50%; margin: auto;}
-			.account__details_col{width: 100% !important;}
-		</style>";
-
-		if(!is_page('affiliates')){
-			wp_redirect(site_url() . "/affiliates");
-				exit;
+	if(is_user_logged_in()){
+		$currentUser = wp_get_current_user();
+		if(sizeof($currentUser->roles) === 1 && in_array('affiliate', $currentUser->roles)){
+			echo "<style>
+				.paused__user_banner{display:none !important;}
+				.account_details__section{width: 50%; margin: auto;}
+				.account__details_col{width: 100% !important;}
+			</style>";
+	
+			if(!is_page('affiliates')){
+				wp_redirect(site_url() . "/affiliates");
+					exit;
+				}
 			}
 		}
 	}
-
 add_action('template_redirect', 'redirectUserToAffiliatesPanel');
 
 
