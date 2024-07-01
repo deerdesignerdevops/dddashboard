26304a5fb23b3415944a88862c940dd2f31c0c03
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-01 12:56:35 -0300

    hidde text on popup form

--- woocommerce/myaccount/my-subscriptions.php
+++ woocommerce/myaccount/my-subscriptions.php
@@ -498,6 +498,8 @@ document.addEventListener("DOMContentLoaded", function(){
 				document.querySelector(".update_plan_form form").elements['form_subscription_plan'].value = currentPlan
 				document.querySelector(".update_plan_form form").elements['form_subscription_update_url'].value = currentUpdatePlanUrl
 				document.querySelector(".update_plan_form form").elements['subscription_url'].value = `<?php echo $siteUrl; ?>/wp-admin/post.php?post=${currentSubscriptionId}&action=edit`
+				document.querySelector('.update_plan_form form').elements['form_subscription_update_message'].style.display = 'none'
+				document.querySelector('.update_plan_form form').elements['select_plans'].style.display = 'block'
 				document.querySelector(".form_subscription_update_disclaimer").style.display = "none"
 			}
 
