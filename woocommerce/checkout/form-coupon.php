33adf79b8e6a3a1e01d08d33ac0a263182f531d1
“malopestorres” <marcuslorenlopes@gmail.com>
2024-09-20 08:04:28 -0300

    changed coupon template

--- woocommerce/checkout/form-coupon.php
+++ woocommerce/checkout/form-coupon.php
@@ -20,10 +20,10 @@ defined( 'ABSPATH' ) || exit;
 if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 	return;
 }
-
 ?>
+
 <div class="woocommerce-form-coupon-toggle">
-	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', esc_html__( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . esc_html__( 'Click here to enter your code', 'woocommerce' ) . '</a>' ), 'notice' ); ?>
+	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', esc_html__( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . esc_html__( 'Click here to enter your code', 'woocommerce' ); ?></a>' ), 'notice' ); ?>
 </div>
 
 <form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
@@ -37,16 +37,23 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 		</p>
 
 		<p class="form-row form-row-last" style="text-align: right;">
-			<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
+			<button type="submit" class="button<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>">
+				<?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?>
+			</button>
 		</p>		
 	</div>
 
 	<?php
-		if( count( WC()->cart->get_applied_coupons() ) > 0 ) {
+		// Exibe os cupons aplicados e uma opção para removê-los
+		if ( count( WC()->cart->get_applied_coupons() ) > 0 ) {
 			$couponsApplied = WC()->cart->get_applied_coupons();
 
-			foreach($couponsApplied as $coupon){ ?>
-				<a class="dd__remove_btn" href="<?php echo site_url(); ?>/signup/?remove_coupon=<?php echo $coupon; ?>">Remove Coupon: <?php echo $coupon; ?></a>
+			foreach( $couponsApplied as $coupon ) { ?>
+				<p class="dd__remove_btn">
+					<a href="<?php echo esc_url( add_query_arg( 'remove_coupon', $coupon, wc_get_cart_url() ) ); ?>" class="button">
+						<?php printf( esc_html__( 'Remove Coupon: %s', 'woocommerce' ), esc_html( $coupon ) ); ?>
+					</a>
+				</p>
 			<?php }
 		}
 	?>
