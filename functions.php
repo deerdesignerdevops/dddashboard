6cd6aa4bc6f8da5de94e74e9e4a9dbbe53c6dfeb
“malopestorres” <marcuslorenlopes@gmail.com>
2024-09-25 19:04:52 -0300

    create_user_values_table

--- functions.php
+++ functions.php
@@ -161,6 +161,26 @@ add_shortcode('message-new-price', 'showSubscriptionMessageIfUserIsNotNewPrice')
     }
 }*/
 
+function create_user_values_table() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'user_values'; 
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        user_id bigint(20) NOT NULL,
+        value float NOT NULL,
+        PRIMARY KEY  (id),
+        UNIQUE KEY user_id (user_id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
+
+add_action('after_setup_theme', 'create_user_values_table');
+
 function checkSubscriptionsPausedOrCancelled($subscription) {
     global $wpdb;
 
